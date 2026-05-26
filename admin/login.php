fe37d3324c98d8bf045be254a2079746d1049b2f
Granados Hernandez Hector Eduardo <22030782@itcelaya.edu.mx>
2026-05-25 21:55:49 -0600

    intento

--- admin/login.php
+++ admin/login.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-// CONTROLADORRRRRmm
+// CONTROLADORRRRRmm,j
 require_once(__DIR__ . "/sistema.class.php");
 
 if (session_status() === PHP_SESSION_NONE) {
