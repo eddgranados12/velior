10d9a67d975cdbdc7d52e59c73e1663150c0971b
Granados Hernandez Hector Eduardo <22030782@itcelaya.edu.mx>
2026-05-25 22:53:51 -0600

    error admin panel

--- admin/login.php
+++ admin/login.php
@@ -1,5 +1,11 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/mi_error.log');
+
 // CONTROLADORRRR
 require_once(__DIR__ . "/sistema.class.php");
 
--- admin/views/login/index.php
+++ admin/views/login/index.php
@@ -35,7 +35,7 @@
             </div>
 
             <div class="forgot-password-link" style="display:flex; justify-content:space-between; gap:10px; flex-wrap:wrap;">
-                <a href="/velior/admin/login.php?accion=recuperar">¿Olvidaste tu contraseñaaaa?</a>
+                <a href="/velior/admin/login.php?accion=recuperar">¿Olvidaste tu contraseña?</a>
                 <a href="/velior/admin/registro.php">No tengo cuenta</a>
                 
             </div>
