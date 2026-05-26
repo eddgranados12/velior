28ce5ead1ae6e75e24f78d3650428fe362ba3e7b
Granados Hernandez Hector Eduardo <22030782@itcelaya.edu.mx>
2026-05-25 23:32:23 -0600

    yaaa xfa

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,9 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/error_debug.log');
 require_once(__DIR__ . "/sistema.class.php");
 require_once(__DIR__ . "/models/DashboardModel.php");
 
